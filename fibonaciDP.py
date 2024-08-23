6c0fc464c280501406d61608e9df74f36bd8790b
VishalxVG <avaratvishal1551@gmail.com>
2024-08-23 22:54:04 +0530

    Initial Commit

---
+++ fibonaciDP.py
@@ -0,0 +1,13 @@
+# Memoization
+
+def fib(n , memo = {}):
+    if n in memo :
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib(n-1 , memo) + fib(n-2 , memo)
+    return memo[n]
+
+n = int(input("Enter Number: "))
+res = fib(n)
+print(f"The {n}th fibonaci number is" , res)
